c29a92fc0c1721d2f0faeab763d4297bf4bb830d
Apexfinity <manspeed721@gmail.com>
2022-04-20 20:29:15 +0000

    semicolons exist

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 
 // Making sure this page isn't hosted somewhere else...
 
-currentPage = windows.location.href
+currentPage = windows.location.href;
 
 if (currentPage.includes('vvxweb.ga') || currentPage.includes('about:blank')) {
 
-console.log('Check passed.')
+console.log('Check passed.');
 
 } else {
     
-    window.location.replace("https://vvxweb.ga")
+    window.location.replace("https://vvxweb.ga");
 
 }

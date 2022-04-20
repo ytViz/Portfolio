bd448969a48446d9615709c08d93741de96164a9
Apexfinity <manspeed721@gmail.com>
2022-04-20 20:22:37 +0000

    added script

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Hello there.
+
+// Making sure this page isn't hosted somewhere else...
+
+currentPage = windows.location.href
+
+if (currentPage.includes('vvxweb.ga') || currentPage.includes('about:blank')) {
+
+console.log('Check passed.')
+
+}
+
+else {
+    
+    window.location.replace("https://vvxweb.ga")
+
+}

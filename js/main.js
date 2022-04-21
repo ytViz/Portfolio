9515fb574242c79d8c3197862ee6fcc0594f3bf1
Apexfinity <manspeed721@gmail.com>
2022-04-21 15:35:15 +0000

    yay script works now added ref

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // Making sure this page isn't hosted somewhere else...
 
-if(document.location.href.indexOf('vvxweb.ga') > -1) { 
+if(document.location.href.indexOf('vvxweb.ga') > -1 || document.location.href.indexOf('about:blank') || document.location.href.indexOf('vvxwebsite.vercel.app')) { 
     console.log('URL STATUS...OK');
 } else {
 

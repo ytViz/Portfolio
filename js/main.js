df11aade491dd1845866f47bca2e7fac1fc58628
Apexfinity <manspeed721@gmail.com>
2022-04-21 19:21:04 +0000

    "its for gardening"

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function launchTheRocket(openSite) {
     win.document.body.style.height = '100vh';
     iframe.style.width = '100%';
     iframe.style.height = '100%';
-    iframe.src = openSite;
+    iframe.src = arguments[1];
     win.document.body.appendChild(iframe);
     window.close();
 }

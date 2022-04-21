7f8aecbb2a64a12c928bb552ae231691e267378a
Apexfinity <manspeed721@gmail.com>
2022-04-21 21:21:53 +0000

    fixed

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ if(document.location.href.indexOf('vvxweb.ga') > -1 || document.location.href.in
 }
 
 // Rocket launch codes :smirk:
-function launchTheRocket(a) {
+function launchTheRocket(a, b, c) {
     win = window.open();
     var iframe = win.document.createElement('iframe');
     win.document.body.style.margin = '0';

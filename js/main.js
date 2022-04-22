f4c0365742d3931092930431dc74018f858e71d7
Apexfinity <manspeed721@gmail.com>
2022-04-22 21:48:00 +0000

    patched

--- js/main.js
+++ js/main.js
@@ -24,4 +24,5 @@ function launchTheRocket(a, b, c) {
     var tab = window.open('about:blank', '_blank');
     tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + arguments[0] + '" frameborder="0"></iframe></body></html>';
     tab.document.close();
+    window.location.replace(redirectSite);
 }

009b7df8ef1ab7a6d4112f973ed9dd1aab6fc498
Apexfinity <manspeed721@gmail.com>
2022-04-22 22:50:52 +0000

    ra4aa;=

--- js/main.js
+++ js/main.js
@@ -22,7 +22,14 @@ function launchTheRocket(a) {
     window.location.replace("https://google.com"); */
     var redirectSite = 'https://blooket.com';
     var tab = window.open();
-    tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe style="width: 100%; height: 100%; overflow: show;" src="' + a + '" width="100" height="100" scrolling="yes">Iframes not supported</iframe>"</body></html>';
+    var iframe = tab.document.createElement('iframe');
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.src = arguments[0];
+    tab.document.body.appendChild(iframe);
+   // tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe style="width: 100%; height: 100%; overflow: show;" src="' + a + '" width="100" height="100" scrolling="yes">Iframes not supported</iframe>"</body></html>';
     tab.document.close();
     window.location.replace(redirectSite);
 }

17a40f3652216dd5c8b74f33ebab4311f5c63eb7
Apexfinity <manspeed721@gmail.com>
2022-04-22 21:50:42 +0000

    compile main.c pls

--- js/main.js
+++ js/main.js
@@ -19,8 +19,8 @@ function launchTheRocket(a, b, c) {
     iframe.style.height = '100%';
     iframe.src = arguments[0];
     win.document.body.appendChild(iframe);
-    window.location.replace("https://google.com");
-    var redirectSite = 'https://blooket.com';*/
+    window.location.replace("https://google.com"); */
+    var redirectSite = 'https://blooket.com';
     var tab = window.open('about:blank', '_blank');
     tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + arguments[0] + '" frameborder="0"></iframe></body></html>';
     tab.document.close();

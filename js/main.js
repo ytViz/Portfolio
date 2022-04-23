c9a09581e9306396373dbb0474ddb0b833d45cd3
Apexfinity <manspeed721@gmail.com>
2022-04-23 15:02:21 +0000

    dude this isn't a node server

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ if (!inFrame() && usingFirefox() != true) {
         }
     } else {
         if (window.location.pathname != "/") {
-            tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + '?url=' + window.location.pathname + '" frameborder="0"></iframe></body></html>';
+            tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + '/' + window.location.pathname + '" frameborder="0"></iframe></body></html>';
         } else {
             tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + '" frameborder="0"></iframe></body></html>';
         }

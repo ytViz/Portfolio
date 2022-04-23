23b3342ff85d35cfc6fd49a2631bcec963c1db0c
Apexfinity <manspeed721@gmail.com>
2022-04-23 14:30:32 +0000

    grrip

--- js/main.js
+++ js/main.js
@@ -10,56 +10,34 @@ if(document.location.href.indexOf('vvxweb.ga') > -1 || document.location.href.in
 }
 
 // Rocket launch codes :smirk:
-function launchTheRocket(a) {
-/*var tab = window.open('about:blank', '_blank');
-  tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + a + '" frameborder="0"></iframe></body></html>';
-  tab.document.close();*/
-  
- function inFrame () {
-  try {
-    return window.self !== window.top;
-  } catch (e) {
-    return true;
-  }
+function launchTheRocket() {
+function inFrame() {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
 }
 
-function usingFirefox(){
-  return navigator.userAgent.indexOf("Firefox") != -1;
+function usingFirefox() {
+    return navigator.userAgent.indexOf("Firefox") != -1;
 }
-
 var redirectSite = "https://www.google.com";
-
-function openBackup(){
-  var tab = window.open('about:blank', '_blank');
-  tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + a + '" frameborder="0"></iframe></body></html>';
-  tab.document.close();
-  
-  window.location.replace(redirectSite);
-}
-
-if( window.location.pathname == "/"){
-  var params = new engine(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-  });
-  if(params.url){
-    var frameUrl = params.url;
-    if(params.url.startsWith("/gams")){
-      frameUrl = "/g" + frameUrl.substring(5);
+if (!inFrame() && usingFirefox() != true) {
+    var tab = window.open('about:blank', '_blank');
+    if (!tab || tab.closed || typeof tab.closed == 'undefined') {
+        console.log("Popup blocked");
+        if (window.location.pathname != "/") {
+            window.location.replace(window.location.origin + '?url=' + window.location.pathname);
+        }
+    } else {
+        if (window.location.pathname != "/") {
+            tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + '?url=' + window.location.pathname + '" frameborder="0"></iframe></body></html>';
+        } else {
+            tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + '" frameborder="0"></iframe></body></html>';
+        }
+        tab.document.close();
+        window.location.replace(redirectSite);
     }
-    document.querySelector(".frame").src = window.location.origin + frameUrl;
-  }
-  if(params.gams = "true"){
-    document.body.style.backgroundImage = "none";
-    document.body.style.backgroundColor = "white";
-  }
-
-  if(inFrame() != true && usingFirefox() != true){
-    document.querySelector(".warning").style.display = "flex";
-    document.querySelector(".tosMsg").style.userSelect = "none";
-    document.onclick = () => {openBackup()};
-    document.onmousedown = () => {openBackup()};
-    document.onkeypress = () => {openBackup()};
-    //document.onkeydown = () => {openBackup()};
-  }
 }
-    }
+}

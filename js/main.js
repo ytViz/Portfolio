979f2cccbfaedcdf5ce3c434d136ef553110c23e
HYPERW1N <100039638+HYPERW1N@users.noreply.github.com>
2022-04-26 11:16:24 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,45 +1,144 @@
-// Hello there.
+/* Credit and Thanks:
+Matrix - Particles.js;
+SliderJS - Ettrics;
+Design - Sara Mazal Web;
+Fonts - Google Fonts
+*/
 
-// Making sure this page isn't hosted somewhere else...
-
-if(document.location.href.indexOf('vvxweb.ga') > -1 || document.location.href.indexOf('about:blank') || document.location.href.indexOf('vvxwebsite.vercel.app')) { 
-    console.log('URL STATUS...OK');
-} else {
-
-    document.location.href = 'http://vvxweb.ga';
-}
-
-// Rocket launch codes :smirk:
-function launchTheRocket() {
-function inFrame() {
-    try {
-        return window.self !== window.top;
-    } catch (e) {
-        return true;
+window.onload = function () {
+  Particles.init({
+    selector: ".background"
+  });
+};
+var particles = Particles.init({
+  selector: ".background",
+  color: ["#03dac6", "#ff0266", "#000000"],
+  connectParticles: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      options: {
+        color: ["#faebd7", "#03dac6", "#ff0266"],
+        maxParticles: 43,
+        connectParticles: false
+      }
     }
-}
+  ]
+});
 
-function usingFirefox() {
-    return navigator.userAgent.indexOf("Firefox") != -1;
-}
-var redirectSite = "https://www.google.com";
-if (!inFrame() && usingFirefox() != true) {
-    var tab = window.open('about:blank', '_blank');
-    if (!tab || tab.closed || typeof tab.closed == 'undefined') {
-        console.log("Popup blocked");
-        if (window.location.pathname != "/") {
-            window.location.replace(window.location.origin + '?url=' + window.location.pathname);
-        }
+class NavigationPage {
+  constructor() {
+    this.currentId = null;
+    this.currentTab = null;
+    this.tabContainerHeight = 70;
+    this.lastScroll = 0;
+    let self = this;
+    $(".nav-tab").click(function () {
+      self.onTabClick(event, $(this));
+    });
+    $(window).scroll(() => {
+      this.onScroll();
+    });
+    $(window).resize(() => {
+      this.onResize();
+    });
+  }
+
+  onTabClick(event, element) {
+    event.preventDefault();
+    let scrollTop =
+      $(element.attr("href")).offset().top - this.tabContainerHeight + 1;
+    $("html, body").animate({ scrollTop: scrollTop }, 600);
+  }
+
+  onScroll() {
+    this.checkHeaderPosition();
+    this.findCurrentTabSelector();
+    this.lastScroll = $(window).scrollTop();
+  }
+
+  onResize() {
+    if (this.currentId) {
+      this.setSliderCss();
+    }
+  }
+
+  checkHeaderPosition() {
+    const headerHeight = 75;
+    if ($(window).scrollTop() > headerHeight) {
+      $(".header").addClass("header--scrolled");
     } else {
-        if (window.location.pathname != "/") {
-            tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + '/' + window.location.pathname + '" frameborder="0"></iframe></body></html>';
-        } else {
-            tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + '" frameborder="0"></iframe></body></html>';
-        }
-        tab.contentWindow.focus();
-        tab.document.close();
-        
-        window.location.replace(redirectSite);
+      $(".header").removeClass("header--scrolled");
     }
+    let offset =
+      $(".nav").offset().top +
+      $(".nav").height() -
+      this.tabContainerHeight -
+      headerHeight;
+    if (
+      $(window).scrollTop() > this.lastScroll &&
+      $(window).scrollTop() > offset
+    ) {
+      $(".header").addClass("et-header--move-up");
+      $(".nav-container").removeClass("nav-container--top-first");
+      $(".nav-container").addClass("nav-container--top-second");
+    } else if (
+      $(window).scrollTop() < this.lastScroll &&
+      $(window).scrollTop() > offset
+    ) {
+      $(".header").removeClass("et-header--move-up");
+      $(".nav-container").removeClass("nav-container--top-second");
+      $(".et-hero-tabs-container").addClass(
+        "et-hero-tabs-container--top-first"
+      );
+    } else {
+      $(".header").removeClass("header--move-up");
+      $(".nav-container").removeClass("nav-container--top-first");
+      $(".nav-container").removeClass("nav-container--top-second");
+    }
+  }
+
+  findCurrentTabSelector(element) {
+    let newCurrentId;
+    let newCurrentTab;
+    let self = this;
+    $(".nav-tab").each(function () {
+      let id = $(this).attr("href");
+      let offsetTop = $(id).offset().top - self.tabContainerHeight;
+      let offsetBottom =
+        $(id).offset().top + $(id).height() - self.tabContainerHeight;
+      if (
+        $(window).scrollTop() > offsetTop &&
+        $(window).scrollTop() < offsetBottom
+      ) {
+        newCurrentId = id;
+        newCurrentTab = $(this);
+      }
+    });
+    if (this.currentId != newCurrentId || this.currentId === null) {
+      this.currentId = newCurrentId;
+      this.currentTab = newCurrentTab;
+      this.setSliderCss();
+    }
+  }
+
+  setSliderCss() {
+    let width = 0;
+    let left = 0;
+    if (this.currentTab) {
+      width = this.currentTab.css("width");
+      left = this.currentTab.offset().left;
+    }
+    $(".nav-tab-slider").css("width", width);
+    $(".nav-tab-slider").css("left", left);
+  }
 }
-}
+
+new NavigationPage();
+
+/* Credit and Thanks:
+Matrix - Particles.js;
+SliderJS - Ettrics;
+Design - Sara Mazal Web;
+Fonts - Google Fonts
+*/
